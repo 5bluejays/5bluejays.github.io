5d32d2708b7760d82e3e7e97da08e9f52d191275
5bluejays <mumby.joshuajm@gmail.com>
2024-06-15 21:01:20 -0600

    Super update crossword.js

--- crossword.js
+++ crossword.js
@@ -1,30 +1,63 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+
+    // Create the day of the week header list
+    var dowheaderlist = [
+        "Sunday - The Midi",
+        "Monday - The Mini",
+        "Tuesday - The Mini",
+        "Wednesday - The Big Mini",
+        "Thursday - The Cryptic",
+        "Friday - The Mini",
+        "Saturday - The Super Cryptic"
+    ];
+
+    // Read file function
+    async function read() {
+        const response = await fetch("crosswords/" + datestr + ".json");
+        const data = await response.json();
+        return data;
+    };
+
     // Get today's date
     var today = new Date();
     var yyyy = today.getFullYear();
     var mm = String(today.getMonth() + 1).padStart(2, "0");
     var dd = String(today.getDate()).padStart(2, "0");
-    
+    var dow = String(today.getDay());
+
     // Construct the file name for today's crossword
     var datestr = dd + "-" + mm + "-" + yyyy;
     var filename = "crosswords/" + datestr + ".png";
-    console.log("Today's date: ", datestr);
-    console.log("Constructed filename: ", filename);
-    
+    console.log("Today's date -", datestr);
+
+    // Create an h3 element to go above the image
+    var h3 = document.createElement("h3");
+    h3.textContent = dowheaderlist[dow];
+
     // Create an img element to go in the html
     var img = document.createElement("img");
-    img.classList.add("crossword");
+    img.classList.add("crosswordimg");
     img.src = filename;
     img.alt = "Today's Crossword";
-    
+
     // If the image doesn't exist, make an alternate message
     img.onerror = function() {
-        document.getElementById("crosswordimg").innerText = "For some reason, the crossword doesn't exist. If you know the creator of this website personally, please notify him about the problem. This is his first time ever using Javascript."
+        document.getElementById("crossword").innerText = "For some reason, the crossword doesn't exist. If you know the creator of this website personally, please notify him about the problem. This is his first time ever using Javascript."
         console.log("The file doesn't exist. ):");
     };
+
+    // Create the across and down clues
+    var across = document.createElement("embed");
+    across.classList.add("crosswordelement");
+    across.src = "crosswords/" + datestr + "A.html";
     
-    // Add the image to the webpage
-    console.log(img);
-    document.getElementById("crosswordimg").appendChild(img)
+    var down = document.createElement("embed");
+    down.classList.add("crosswordelement");
+    down.src = "crosswords/" + datestr + "D.html"
+
+    // Add everything to the webpage
+    document.getElementById("crosswordheader").appendChild(h3);
+    document.getElementById("crossword").appendChild(img);
+    document.getElementById("crossword").appendChild(across);
+    document.getElementById("crossword").appendChild(down);
 });
